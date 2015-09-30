4b2e669340604a480424094a97bb8efd482c073e
Jason Tam <jason@wishpond.com>
2015-09-29 19:23:41 -0800

    Add create and edit toggling with setting states

--- app/derp-app.js
+++ app/derp-app.js
@@ -27,13 +27,51 @@ angular.module('Derp', [
 
   function setCurrentCategory(category) {
     $scope.currentCategory = category;
+    cancelCreating();
+    cancelEditing();
   }
 
   function isCurrentCategory(category) {
     $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
   }
 
-  $scope.setCurrentCategory = setCurrentCategory
-  $scope.isCurrentCategory = isCurrentCategory
+  $scope.setCurrentCategory = setCurrentCategory;
+  $scope.isCurrentCategory  = isCurrentCategory;
+
+  $scope.isCreating = false;
+  $scope.isEditing  = false;
+
+  function startCreating() {
+    $scope.isCreating = true;
+    $scope.isEditing  = false;
+  }
+
+  function cancelCreating() {
+    $scope.isCreating = false;
+  }
+
+  function startEditing() {
+    $scope.isCreating = false;
+    $scope.isEditing  = true;
+  }
+
+  function cancelEditing() {
+    $scope.isEditing = false;
+  }
+
+  function shouldShowCreating() {
+    return $scope.currentCategory && !$scope.isEditing;
+  }
+
+  function shouldShowEditing() {
+    return $scope.isEditing && !$scope.isCreating
+  }
+
+  $scope.startCreating      = startCreating;
+  $scope.cancelCreating     = cancelCreating;
+  $scope.startEditing       = startEditing;
+  $scope.cancelEditing      = cancelEditing;
+  $scope.shouldShowCreating = shouldShowCreating;
+  $scope.shouldShowEditing  = shouldShowEditing;
 
 });
